4853a87eb37d1dd143a4eda3baa5f89a47a45eeb
dzejkobjj <dzejkobjj@gmail.com>
2017-03-30 16:17:56 +0200

    Caly program sie normalnie zamyka

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,14 +1,17 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application{
 
-    public Scene scene;
+    private Scene scene;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -17,6 +20,11 @@ public class Main extends Application{
 
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
     }
 
     public static void main(String[] args) {
